845c8c43d0875a20af7158a9c95af70039c51802
mehran <mehran22000@yahoo.com>
2018-03-23 19:05:15 -0400

    added friends ranking

--- routes/v1/holders/stock.js
+++ routes/v1/holders/stock.js
@@ -9,12 +9,18 @@ var market;
 var _db;
 
 
-router.get('/global/:symbol', function(req, res) {
+router.get('/:symbol/global/:global/userid/:userid', function(req, res) {
    var _db;
    var holders = [];
    var users = [];
    var rankings = [];
    var symbol = req.params.symbol;
+   var global = req.params.global;
+   var user_id = req.params.userid;
+   var friends_str_sync;
+   var friends_counter;
+   var friends_no;
+   
    var results = [];
    	 		
 	mongoClient.connectAsync(req.db_url)  
@@ -36,18 +42,41 @@ router.get('/global/:symbol', function(req, res) {
 	.then(function(_rankings) {
 		rankings = _rankings;
 		
+		if (global == 'false') {
+			var user_index = find_user_index(users,user_id);
+			friends_str_sync = ',' + users[user_index].friends + ',';
+			var user_friends_array = users[user_index].friends.split(',');
+    		friends_no = user_friends_array.length;
+			friend_counter = 0;
+		}
+		
+			
 		for (var h in holders)
     	{
     		console.log(holders[h]);
     		var user_index = find_user_index(users,holders[h].user_id);
     		var rank_index = find_user_index(rankings,holders[h].user_id);
     		if (user_index >= 0) {
-    			console.log(user_index);
+    			
     			holders[h].first_name = users[user_index].first_name;
     			holders[h].last_name = users[user_index].last_name;
     			holders[h].photo_url = users[user_index].photo_url;
     			holders[h].global_ranking = rankings[rank_index].rank_global;
-    			results.push(holders[h]);
+    			
+    			
+    			if (global == 'false') {
+    				var search_key = ',' + users[user_index].user_id + ',';
+    				if (friends_str_sync.indexOf(search_key) !== -1) {
+    					results.push(holders[h]);
+    					friend_counter = friend_counter + 1;
+    					if (friend_counter >= friends_no) {
+    						break;
+    					}
+    				}
+    			}
+    			else {
+    				results.push(holders[h]);
+    			}
     		}
     	}
     	
